9998e4ac6836dfc1b14891a730f4f415fffcabb1
Eliezer Wohl <eliezerwohl@gmail.com>
2017-07-04 16:39:18 -0400

    successfully logged in

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,1 +1,11 @@
-api.js
+const express = require('express');
+
+const router = new express.Router();
+
+router.get('/dashboard', (req, res) => {
+  res.status(200).json({
+    message: "You're authorized to see this secret message."
+  });
+});
+
+module.exports = router;
